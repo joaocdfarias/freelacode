56559fb4d4dd263edeaba522c5fc3e1a8448053b
João Carlos <joacdfariass@gmail.com>
2021-03-20 23:30:03 -0300

    added: GlobalStyle

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { ThemeProvider } from "styled-components";
 import light from "./Themes/light";
 import Header from "./Header";
 import Banner from "./Banner";
+import { GlobalStyle } from "./GlobalStyle";
 
 function App() {
   return (
     <ThemeProvider theme={light}>
+      <GlobalStyle />
       <Container maxWidth="lg">
         <Header />
         <Banner />
